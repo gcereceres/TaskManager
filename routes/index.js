766295e6b2208e489353399b37b2c8f3001e6549
Gustavo Cereceres <gcereceres@gmail.com>
2018-01-13 13:44:59 -0700

    Adding application root and routes file

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+module.exports = () => {
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        res.send('Hello Nearsoft!');
+    })
+
+    return router;
+};
